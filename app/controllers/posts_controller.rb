e731c962f6a9709dbc1cf12e475c435ef54f5e2a
MaruyamaNao <61174627+MaruyamaNao@users.noreply.github.com>
2020-03-18 07:48:16 +0900

    編集機能パス変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,9 +30,9 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to root_path
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
   end
 
   private
